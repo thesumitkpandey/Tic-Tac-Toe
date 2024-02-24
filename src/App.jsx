22127e8a04732f75a25fa2f9b79ca70d779f47cf
Sumit Kumar Pandey <officialsumitpandey@gmail.com>
2024-02-24 16:47:49 +0530

    Player name feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./Components/Player.jsx";
+import logo from "../public/logo.jpg";
+import Game from "./Components/Game.jsx";
+import "./App.css";
 export default function App(){
-  return (
-    <h1>Coming soon ...</h1>
-  )
+    return (
+        <div className="container">
+            <img src={logo} className="logo" alt="logo"/>
+            <div className="game-container">
+                <div className="players-section">
+                    <Player initialPlayerName="Player X"/>
+                    <Player initialPlayerName="Player O"/>
+                </div>
+                <Game/>
+            </div>
+        </div>
+    )
 }
---
+++ src/Components/Player.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export default function Player({ initialPlayerName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialPlayerName);
+    const [btnName, setBtnName] = useState("Edit");
+
+    function handleNameChange(e) {
+        setPlayerName(e.target.value);
+    }
+
+    function handleNameEdit() {
+        if (btnName == "Edit") {
+            setBtnName("Save");
+        } else {
+            setBtnName("Edit");
+        }
+    }
+
+    return (
+        <div className="players">
+            <input
+                className="player-name"
+                type="text"
+                value={playerName}
+                onChange={handleNameChange}
+                readOnly={btnName === "Edit"}
+            />
+            <div className="player-symbol">{symbol}</div>
+            <button onClick={handleNameEdit}>{btnName}</button>
+        </div>
+    );
+}
